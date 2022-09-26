8378ce5c32e8c2a6bba69b6cd655a88a909b4af0
wan-maoyuan <wanmaoyuan@outlook.com>
2022-09-26 10:34:55 +0800

    add project info

--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"chip-distribution-go/pkg/server"
+	"fmt"
 
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	Name       string = "chip-distribution"
+	Version    string //版本
+	CommitHash string //git 提交的 hash 值
+	BuildTime  string //编译时间
+)
+
 func main() {
+	showVersion()
+
 	svr, err := server.NewChipServer(":8080")
 	if err != nil {
 		logrus.Fatalf("create chip server error: %v", err)
 	}
 
 	svr.Run()
+}
+
+func showVersion() {
+	fmt.Printf(`
+		Name: %s
+		Version: %s
+		CommitHash: %s
+		BuildTime: %s
+
+`, Name, Version, CommitHash, BuildTime)
 }
